c88bdfbe537dbdf9b4441c2c09e326050d83703a
Michael Rowsey <mike@rowsey.org>
2026-06-21 20:17:11 -0700

    reformat weird algorithm

--- src/cses/weird_algorithm.py
+++ src/cses/weird_algorithm.py
@@ -2,21 +2,19 @@ def solve(n: int) -> str:
     result = []
     while True:
         result.append(str(n))
-
         if n == 1:
             break
-
         if n & 1 == 0:
             n //= 2
         else:
             n = 3 * n + 1
-
     return " ".join(result)
 
 
 def main():
     n = int(input())
     print(solve(n))
+
 
 if __name__ == "__main__":
     main()
