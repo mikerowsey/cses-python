f620bdcae3cdd7da84c1baed4099ef7b38eac951
Michael Rowsey <mike@rowsey.org>
2026-06-23 00:32:38 -0700

    weird_algorithm simplified

--- src/cses/weird_algorithm.py
+++ src/cses/weird_algorithm.py
@@ -1,20 +1,14 @@
-def solve(n: int) -> str:
-    result = []
-    while True:
-        result.append(str(n))
-        if n == 1:
-            break
-        if n & 1 == 0:
-            n //= 2
-        else:
-            n = 3 * n + 1
-    return " ".join(result)
+import sys
 
+n = int(input())
+result = []
+while True:
+    result.append(str(n))
+    if n == 1:
+        break
+    if n & 1 == 0:
+        n //= 2
+    else:
+        n = 3 * n + 1
 
-def main():
-    n = int(input())
-    print(solve(n))
-
-
-if __name__ == "__main__":
-    main()
+sys.stdout.write(" ".join(result))
