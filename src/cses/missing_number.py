bcdc1c2bd35879ce8e35337d7f8f71991bd69b09
Michael Rowsey <mike@rowsey.com>
2026-06-24 05:28:40 -0700

    switch to cses template, always send it to solve, always return string to  main. Convert weird_algorithm.py & missing_number.py to new format. Add jupyter for easier testing.

--- src/cses/missing_number.py
+++ src/cses/missing_number.py
@@ -1,10 +1,29 @@
 import sys
 
-it = iter(sys.stdin.buffer.read().split())
-ni = lambda: int(next(it))
 
-n = ni()
-expected_sum = n * (n + 1) // 2
-actual_sum = sum(map(int, it))
+def ni(it) -> int:
+    return int(next(it))
 
-print(expected_sum - actual_sum)
+
+def nil(it, n: int) -> list[int]:
+    return [ni(it) for _ in range(n)]
+
+
+def ns(it) -> str:
+    return next(it).decode()
+
+
+def solve(it) -> str:
+    n = ni(it)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(map(int, it))
+    return str(expected_sum - actual_sum)
+
+
+def main() -> None:
+    it = iter(sys.stdin.buffer.read().split())
+    sys.stdout.write(solve(it) + "\n")
+
+
+if __name__ == "__main__":
+    main()
--- src/cses/weird_algorithm.py
+++ src/cses/weird_algorithm.py
@@ -1,14 +1,36 @@
 import sys
 
-n = int(input())
-result = []
-while True:
-    result.append(str(n))
-    if n == 1:
-        break
-    if n & 1 == 0:
-        n //= 2
-    else:
-        n = 3 * n + 1
 
-sys.stdout.write(" ".join(result))
+def ni(it) -> int:
+    return int(next(it))
+
+
+def nil(it, n: int) -> list[int]:
+    return [ni(it) for _ in range(n)]
+
+
+def ns(it) -> str:
+    return next(it).decode()
+
+
+def solve(it) -> str:
+    result = []
+    n = ni(it)
+    while True:
+        result.append(str(n))
+        if n == 1:
+            break
+        if n & 1 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+    return " ".join(result)
+
+
+def main() -> None:
+    it = iter(sys.stdin.buffer.read().split())
+    sys.stdout.write(solve(it) + "\n")
+
+
+if __name__ == "__main__":
+    main()
