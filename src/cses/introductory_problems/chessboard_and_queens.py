3fafb33b3460abe35bb16dc6462929b1fbc600b7
Michael Rowsey <mike@rowsey.org>
2026-06-25 23:21:22 -0700

    chessboard & queens - WIP

---
+++ src/cses/introductory_problems/chessboard_and_queens.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def solve(it) -> str:
+    board = [ns(it) for _ in range(8)]
+    return "\n".join(board)
+
+
+def ni(it) -> int:
+    return int(next(it))
+
+
+def nil(it, n: int) -> list[int]:
+    return [ni(it) for _ in range(n)]
+
+
+def ns(it) -> str:
+    return next(it).decode()
+
+
+def main() -> None:
+    it = iter(sys.stdin.buffer.read().split())
+    sys.stdout.write(solve(it) + "\n")
+
+
+if __name__ == "__main__":
+    main()
